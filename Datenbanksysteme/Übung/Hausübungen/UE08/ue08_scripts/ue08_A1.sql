a0ac3189a6a2b85d0a0d1b5609a57a114b5ce3d3
klemensdanner <klemens.danner@gmx.at>
2026-05-18 12:03:02 +0200

    vault backup: 2026-05-18 12:03:01

--- Datenbanksysteme/Übung/Hausübungen/UE08/ue08_scripts/ue08_A1.sql
+++ Datenbanksysteme/Übung/Hausübungen/UE08/ue08_scripts/ue08_A1.sql
@@ -7,5 +7,44 @@ SELECT employee_id AS EmpID,
 
 
 SELECT DISTINCT manager_id 
-FROM employees;
+FROM employees
 WHERE manager_id IS NOT NULL;
+
+
+SELECT *
+FROM departments
+WHERE manager_id = 201;
+
+-- 4
+SELECT last_name, salary
+FROM employees 
+WHERE salary > 6000;
+
+
+-- 5
+SELECT last_name, salary, department_id
+FROM employees
+WHERE employee_id = 124;
+
+--6
+SELECT last_name, job_id, hire_date
+FROM employees
+ORDER BY hire_date ASC;
+
+--7
+SELECT last_name, department_id
+FROM employees
+WHERE department_id = 20
+ORDER BY last_name ASC;
+
+
+--8
+SELECT last_name AS Employee, salary AS MonthlySalary, commission_pct AS Commission
+FROM employees
+WHERE commission_pct = 20 OR job_id = 'SA_REP';
+
+
+--9
+SELECT first_name AS name, last_name AS surname, employee_id, salary -- restliche Namen sind passend
+FROM employees
+WHERE department_id = 60 AND manager_id = 103;
